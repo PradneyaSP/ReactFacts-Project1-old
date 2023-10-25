469e3035978785669699a7ab52128edbe57f7dcb
Pradneya <pradneya.prog@gmail.com>
2023-10-25 10:03:22 +0530

    Completed the Project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="react-logo">
+        <img src="D:\Web Development\ReactJS\project1\src\logo.png" alt="react-logo" />
+        <h3>ReactFacts</h3>
+      </div>
+      <div className="name">
+        <p>React Course - Project 1</p>
+      </div>
+    </nav>
+  );
+}
